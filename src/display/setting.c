b82858176f19982111fad02d78f4618e0c16c7e7
sh.pi <sh.pi@samsung.com>
2013-05-02 19:45:26 +0900

    Remove LCD 'always on' operation by vconf.
    vconf value 0 is not used for 'always on' anymore.
    then backlight time 0 is invalid value now.
    
    Change-Id: I2fa53e18b72b8a5b8bf04e2ac20d78d06b6d1798

--- src/display/setting.c
+++ src/display/setting.c
@@ -19,6 +19,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #include "util.h"
 #include "setting.h"
@@ -90,10 +91,11 @@ int get_run_timeout(int *timeout)
 
 	ret = vconf_get_int(setting_keys[SETTING_TO_NORMAL], &vconf_timeout);
 
-	if(vconf_timeout == 0)
-		*timeout = 0; //timeout 0 : Always ON (Do not apply dim_timeout)
+	if (ret < 0 || vconf_timeout <= 0)
+		ret = -ERANGE;
 	else
 		*timeout = vconf_timeout - dim_timeout;
+
 	return ret;
 
 }
