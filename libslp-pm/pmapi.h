267ade6bae33d3322ef957309041b78138139530
jy910.yun <jy910.yun@samsung.com>
2013-06-24 14:28:27 +0900

    embody 'pm' in libdeviced.

--- libslp-pm/pmapi.h
+++ libslp-pm/pmapi.h
@@ -94,22 +94,24 @@ int main(int argc, char** argv)
 extern "C" {
 #endif
 
-#define LCD_NORMAL	0x1   /**< NORMAL state */
-#define LCD_DIM		0x2  /**< LCD dimming state */
-#define LCD_OFF		0x4  /**< LCD off state */
-#define SUSPEND		0x8  /**< Sleep state */
-#define POWER_OFF	0x16  /**< Sleep state */
-#define SETALL (LCD_DIM | LCD_OFF | LCD_NORMAL)	/*< select all state - not supported yet */
+#ifndef __DD_DISPLAY_H__
+#define LCD_NORMAL     0x1   /**< NORMAL state */
+#define LCD_DIM                0x2  /**< LCD dimming state */
+#define LCD_OFF                0x4  /**< LCD off state */
+#define SUSPEND                0x8  /**< Sleep state */
+#define POWER_OFF      0x16  /**< Sleep state */
+#define SETALL (LCD_DIM | LCD_OFF | LCD_NORMAL)        /*< select all state - not supported yet */
 
 /* parameters for pm_lock_state() */
-#define STAY_CUR_STATE	0x0
-#define GOTO_STATE_NOW	0x1
-#define HOLD_KEY_BLOCK	0x2
+#define STAY_CUR_STATE 0x0
+#define GOTO_STATE_NOW 0x1
+#define HOLD_KEY_BLOCK 0x2
 
 /* paramters for pm_unlcok_state() - details are described at 162 line */
-#define PM_SLEEP_MARGIN	0x0	/**< keep guard time for unlock */
-#define PM_RESET_TIMER	0x1	/**< reset timer for unlock */
-#define PM_KEEP_TIMER	0x2	/**< keep timer for unlock */
+#define PM_SLEEP_MARGIN        0x0     /**< keep guard time for unlock */
+#define PM_RESET_TIMER 0x1     /**< reset timer for unlock */
+#define PM_KEEP_TIMER  0x2     /**< keep timer for unlock */
+#endif
 
 /**
  * @fn int pm_lock_state	(unsigned int state,
