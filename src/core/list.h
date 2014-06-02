42c89ce62a714788ce3066d0b5a3e73c1c4752ea
Maciej Wereski <m.wereski@partner.samsung.com>
2014-06-02 16:40:49 +0200

    Add dd_list
    The code was written by: SeungHun Pi, Jiyoung Yun and Taeyoung Kim
    
    Change-Id: Ifef7af0cc54cea7811966cf2683e2f5d7f5207e5
    Signed-off-by: Maciej Wereski <m.wereski@partner.samsung.com>

--- src/core/list.h
+++ src/core/list.h
@@ -33,4 +33,47 @@
 #define EINA_LIST_FREE_LIST(a) \
 	a = eina_list_free(a)
 
+#ifdef EINA_LIST
+typedef Eina_List dd_list;
+#define DD_LIST_PREPEND(a, b)   \
+	        a = eina_list_prepend(a, b)
+#define DD_LIST_APPEND(a, b)    \
+	        a = eina_list_append(a, b)
+#define DD_LIST_REMOVE(a, b)    \
+	        a = eina_list_remove(a, b)
+#define DD_LIST_LENGTH(a)               \
+	        eina_list_count(a)
+#define DD_LIST_NTH(a, b)                       \
+	        eina_list_nth(a, b)
+#define DD_LIST_FREE_LIST(a)    \
+	        a = eina_list_free(a)
+#define DD_LIST_FOREACH(head, elem, node)       \
+	        EINA_LIST_FOREACH(head, elem, node)
+#define DD_LIST_FOREACH_SAFE(head, elem, elem_next, node) \
+	        EINA_LIST_FOREACH_SAFE(head, elem, elem_next, node)
+
+#else
+#include <glib.h>
+typedef GList dd_list;
+#define DD_LIST_PREPEND(a, b)           \
+	        a = g_list_prepend(a, (gpointer)b)
+#define DD_LIST_APPEND(a, b)            \
+	        a = g_list_append(a, (gpointer)b)
+#define DD_LIST_REMOVE(a, b)            \
+	        a = g_list_remove(a, (gpointer)b)
+#define DD_LIST_LENGTH(a)                       \
+	        g_list_length(a)
+#define DD_LIST_NTH(a, b)                       \
+	        g_list_nth_data(a, b)
+#define DD_LIST_FREE_LIST(a)        \
+	        g_list_free(a)
+#define DD_LIST_FOREACH(head, elem, node)       \
+	        for (elem = head, node = NULL; elem && ((node = elem->data) != NULL); elem = elem->next, node = NULL)
+#define DD_LIST_FOREACH_SAFE(head, elem, elem_next, node) \
+	        for (elem = head, elem_next = g_list_next(elem), node = NULL; \
+				                        elem && ((node = elem->data) != NULL); \
+				                        elem = elem_next, elem_next = g_list_next(elem), node = NULL)
+
+#endif
+
 #endif
