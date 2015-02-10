9705920dcbac591bb2ea40ba45b6cab012f9f2fc
taeyoung <ty317.kim@samsung.com>
2015-02-10 16:54:30 +0900

    usb: add plugin interfaces for usb configuration
    - USB configuration manner can be changed according to the Kernel or vendors.
      Thus USB configuration setting codes should be implemented by plugin structure.
    - This modification contains basic structure for the USB configuration plugin.
    
    Change-Id: I4afb1033fb59ba132182ae16ffef9fbb34192f13
    Signed-off-by: taeyoung <ty317.kim@samsung.com>

--- src/usb/usb.c
+++ src/usb/usb.c
@@ -20,21 +20,133 @@
 #include <stdio.h>
 
 #include "core/log.h"
+#include "core/list.h"
 #include "core/common.h"
 #include "core/device-notifier.h"
 #include "extcon/extcon.h"
+#include "usb.h"
 
 enum usb_state {
 	USB_DISCONNECTED,
 	USB_CONNECTED,
 };
 
+static dd_list *config_list;
 struct extcon_ops extcon_usb_ops;
+static const struct usb_config_plugin_ops *config_plugin;
+
+void add_usb_config(const struct usb_config_ops *ops)
+{
+	DD_LIST_APPEND(config_list, ops);
+}
+
+void remove_usb_config(const struct usb_config_ops *ops)
+{
+	DD_LIST_REMOVE(config_list, ops);
+}
+
+static int usb_config_module_load(void)
+{
+	dd_list *l;
+	struct usb_config_ops *ops;
+
+	DD_LIST_FOREACH(config_list, l, ops) {
+		if (ops->is_valid && ops->is_valid()) {
+			if (ops->load)
+				config_plugin = ops->load();
+			return 0;
+		}
+	}
+	return -ENOENT;
+}
+
+static void usb_config_module_unload(void)
+{
+	dd_list *l;
+	struct usb_config_ops *ops;
+
+	config_plugin = NULL;
+
+	DD_LIST_FOREACH(config_list, l, ops) {
+		if (ops->is_valid && ops->is_valid()) {
+			if (ops->release)
+				ops->release();
+		}
+	}
+}
+
+static int usb_config_init(void)
+{
+	if (!config_plugin) {
+		_E("There is no usb config plugin");
+		return -ENOENT;
+	}
+
+	if (config_plugin->init == NULL) {
+		_E("There is no usb config init function");
+		return -ENOENT;
+	}
+
+	/* TODO:
+	 * parameter "DEFAULT" can be changed */
+	return config_plugin->init("DEFAULT");
+}
+
+static void usb_config_deinit(void)
+{
+	if (!config_plugin) {
+		_E("There is no usb config plugin");
+		return;
+	}
+
+	if (config_plugin->deinit == NULL) {
+		_E("There is no usb config deinit function");
+		return;
+	}
+
+	/* TODO:
+	 * parameter "DEFAULT" can be changed */
+	config_plugin->deinit("DEFAULT");
+}
+
+static int usb_config_enable(void)
+{
+	if (!config_plugin) {
+		_E("There is no usb config plugin");
+		return -ENOENT;
+	}
+
+	if (config_plugin->enable == NULL) {
+		_E("There is no usb config enable function");
+		return -ENOENT;
+	}
+
+	/* TODO:
+	 * parameter "DEFAULT" can be changed */
+	return config_plugin->enable("DEFAULT");
+}
+
+static int usb_config_disable(void)
+{
+	if (!config_plugin) {
+		_E("There is no usb config plugin");
+		return -ENOENT;
+	}
+
+	if (config_plugin->disable == NULL) {
+		_E("There is no usb config disable function");
+		return -ENOENT;
+	}
+
+	/* TODO:
+	 * parameter "DEFAULT" can be changed */
+	return config_plugin->disable("DEFAULT");
+}
 
 static int usb_state_changed(void *data)
 {
 	static int state = USB_DISCONNECTED;
-	int input;
+	int input, ret;
 
 	if (!data)
 		return -EINVAL;
@@ -49,25 +161,39 @@ static int usb_state_changed(void *data)
 	switch (input) {
 	case USB_CONNECTED:
 		_I("USB cable is connected");
+		ret = usb_config_enable();
 		break;
 	case USB_DISCONNECTED:
 		_I("USB cable is disconnected");
+		ret = usb_config_disable();
 		break;
 	default:
 		_E("Invalid USB state(%d)", state);
 		return -EINVAL;
 	}
+	if (ret < 0)
+		_E("Failed to operate usb connection(%d)", ret);
+	else
+		state = input;
 
-	state = input;
-
-	return 0;
+	return ret;
 }
 
 static void usb_init(void *data)
 {
-	int ret, status;
+	int ret;
 
 	register_notifier(DEVICE_NOTIFIER_USB, usb_state_changed);
+
+	ret = usb_config_module_load();
+	if (ret < 0) {
+		_E("Failed to get config module (%d)", ret);
+		return;
+	}
+
+	ret = usb_config_init();
+	if (ret < 0)
+		_E("Failed to initialize usb configuation");
 
 	ret = usb_state_changed(&(extcon_usb_ops.status));
 	if (ret < 0)
@@ -77,6 +203,8 @@ static void usb_init(void *data)
 static void usb_exit(void *data)
 {
 	unregister_notifier(DEVICE_NOTIFIER_USB, usb_state_changed);
+	usb_config_deinit();
+	usb_config_module_unload();
 }
 
 struct extcon_ops extcon_usb_ops = {
