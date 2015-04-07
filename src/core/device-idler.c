d32f876fdb862f41700a06581cdcc6f67a8cd98a
Jiyoung Yun <jy910.yun@samsung.com>
2015-04-07 15:21:41 +0900

    deviced: Add device idler logic
    It allows for callbacks to be called when the daemon is idle state.
    func:
      int add_idle_request(int (*func)(void *data), void *data);
    
    Change-Id: I2d7879874da5bd072b6ed205ff4d33a81e919dc7
    Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>

---
+++ src/core/device-idler.c
@@ -0,0 +1,102 @@
+/*
+ * deviced
+ *
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include <Ecore.h>
+#include <glib.h>
+#include <errno.h>
+
+#include "log.h"
+
+struct device_request {
+	int (*func)(void *data);
+	void *data;
+};
+
+static GQueue req_queue = G_QUEUE_INIT;
+static Ecore_Idler *idler;
+
+static int free_request(struct device_request *req)
+{
+	if (!req)
+		return -EINVAL;
+
+	free(req);
+	return 0;
+}
+
+static Eina_Bool idler_cb(void *data)
+{
+	struct device_request *req;
+
+	req = g_queue_pop_head(&req_queue);
+	if (req) {
+		if (req->func)
+			req->func(req->data);
+		free_request(req);
+	}
+
+	if (g_queue_is_empty(&req_queue)) {
+		idler = NULL;
+		return ECORE_CALLBACK_CANCEL;
+	}
+
+	return ECORE_CALLBACK_RENEW;
+}
+
+static void process_next_request_in_idle(void)
+{
+	if (g_queue_is_empty(&req_queue))
+		return;
+
+	if (idler)
+		return;
+
+	idler = ecore_idler_add(idler_cb, NULL);
+	/**
+	 * if idler is null,
+	 * it means whole system might be an abnormal state.
+	 * so it just prints out error log.
+	 */
+	if (!idler)
+		_E("fail to add request to idler");
+}
+
+int add_idle_request(int (*func)(void *data), void *data)
+{
+	struct device_request *req;
+
+	if (!func) {
+		_E("invalid argumet : func(NULL)");
+		return -EINVAL;
+	}
+
+	req = calloc(1, sizeof(struct device_request));
+	if (!req) {
+		_E("fail to allocate request : %s", strerror(errno));
+		return -errno;
+	}
+
+	req->func = func;
+	req->data = data;
+
+	g_queue_push_tail(&req_queue, req);
+	process_next_request_in_idle();
+
+	return 0;
+}
