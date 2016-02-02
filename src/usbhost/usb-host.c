73e6234d3076aad7dda534a4f4cec115467bd5a2
Paweł Szewczyk <p.szewczyk@samsung.com>
2016-02-02 18:53:54 +0100

    usbhost: Store policy in file
    Policy is stored in one file at deviced shutdown and read from it at
    startup
    
    Change-Id: I8a5ef1da3c44d7b4757ac4e16a07f86863e09c3a
    Signed-off-by: Paweł Szewczyk <p.szewczyk@samsung.com>

--- src/usbhost/usb-host.c
+++ src/usbhost/usb-host.c
@@ -41,6 +41,8 @@
 #define SIGNAL_USB_HOST_CHANGED "ChangedDevice"
 #define METHOD_GET_CONNECTION_CREDENTIALS "GetConnectionCredentials"
 
+#define POLICY_FILEPATH "/var/usbhost-policy"
+
 /**
  * Below usb host class is defined by www.usb.org.
  * Please refer to below site.
@@ -489,6 +491,127 @@ static const char *policy_value_str(enum policy_value value) {
 	}
 }
 
+static int get_policy_value_from_str(const char *str) {
+	if (strcmp("ALLOW", str) == 0)
+		return POLICY_ALLOW;
+	if (strcmp("DENY", str) == 0)
+		return POLICY_DENY;
+	return -1;
+}
+
+static int store_policy()
+{
+	int fd;
+	dd_list *elem;
+	struct policy_entry *entry;
+	char line[256];
+	int ret;
+
+	fd = open(POLICY_FILEPATH, O_WRONLY | O_CREAT, 0664);
+	if (fd < 0) {
+		_E("Could not open policy file for writing: %m");
+		ret = -1;
+		goto out;
+	}
+
+	DD_LIST_FOREACH(access_list, elem, entry) {
+		ret = snprintf(line, 256, "%d %s %04x %02x %02x %02x %04x %04x %04x %s\n",
+				entry->creds.uid,
+				entry->creds.sec_label,
+				entry->device.bcdUSB,
+				entry->device.bDeviceClass,
+				entry->device.bDeviceSubClass,
+				entry->device.bDeviceProtocol,
+				entry->device.idVendor,
+				entry->device.idProduct,
+				entry->device.bcdDevice,
+				policy_value_str(entry->value));
+		if (ret < 0) {
+			_E("Serialization failed: %m");
+			goto out;
+		}
+
+		ret = write(fd, line, ret);
+		if (ret < 0) {
+			_E("Error writing policy entry: %m");
+			goto out;
+		}
+	}
+
+	_I("Policy stored in %s", POLICY_FILEPATH);
+
+	ret = 0;
+
+out:
+	close(fd);
+	return ret;
+}
+
+static int read_policy()
+{
+	FILE *fp;
+	dd_list *elem;
+	struct policy_entry *entry;
+	char line[256], value_str[256];
+	char *p;
+	int ret = -1;
+	int count = 0;
+	int len;
+
+	fp = fopen(POLICY_FILEPATH, "r");
+	if (!fp) {
+		_E("Could not open policy file for reading: %m");
+		return -EACCES;
+	}
+
+	while ((ret = getline(&line, &len, fp)) != -1) {
+		entry = malloc(sizeof(*entry));
+		if (!entry) {
+			_E("No memory: %m");
+			goto out;
+		}
+
+		entry->creds.sec_label = calloc(256, 1);
+		if (!entry->creds.sec_label) {
+			_E("No memory: %m");
+			goto out;
+		}
+
+		ret = sscanf(line, "%d %s %04x %02x %02x %02x %04x %04x %04x %s\n",
+				&entry->creds.uid,
+				entry->creds.sec_label,
+				&entry->device.bcdUSB,
+				&entry->device.bDeviceClass,
+				&entry->device.bDeviceSubClass,
+				&entry->device.bDeviceProtocol,
+				&entry->device.idVendor,
+				&entry->device.idProduct,
+				&entry->device.bcdDevice,
+				value_str);
+
+		entry->value = get_policy_value_from_str(value_str);
+		if (entry->value < 0) {
+			_E("Invalid policy value: %s", value_str);
+			ret = -EINVAL;
+			goto out;
+		}
+
+		_I("%04x:%04x : %s", entry->device.idVendor, entry->device.idProduct,
+				value_str);
+
+		DD_LIST_APPEND(access_list, entry);
+		count++;
+	}
+
+	_I("Found %d policy entries", count);
+	ret = 0;
+
+out:
+	fclose(fp);
+
+	return ret;
+}
+
 static int get_device_desc(const char *filepath, struct usb_device_descriptor *desc)
 {
 	char *path = NULL;
@@ -604,6 +727,8 @@ static int get_policy_value(const char *path, struct user_credentials *cred)
 
 	_I("Added policy entry: %s", policy_value_str(entry->value));
 	DD_LIST_APPEND(access_list, entry);
+
+	store_policy();
 
 	return entry->value;
 }
@@ -818,6 +943,8 @@ static void usbhost_init(void *data)
 
 	/* register notifier */
 	register_notifier(DEVICE_NOTIFIER_BOOTING_DONE, booting_done);
+
+	read_policy();
 }
 
 static void usbhost_exit(void *data)
@@ -832,6 +959,7 @@ static void usbhost_exit(void *data)
 	/* remove all usbhost list */
 	remove_all_usbhost_list();
 
+	store_policy();
 	remove_all_access_list();
 }
 
