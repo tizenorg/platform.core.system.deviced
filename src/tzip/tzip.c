10f610403baa9d4437bca2e438e3dd2b9f666779
Taeyoung Kim <ty317.kim@samsung.com>
2016-04-19 16:55:56 +0900

    tzip: fix issues during large file seek and read operations
    1. Previously read data is used during seek for subsequent read request.
       This minimizes duplicate parsing and reading from ZIP file.
    2. Lock during read operation is added.
    3. Reset_zipfile function is added since unzSetOffset()
       does not reset file internal values.
    
    Change-Id: I69cc23a0a49bc320eca8d1093bb9633430b13152
    Signed-off-by: Taeyoung Kim <ty317.kim@samsung.com>

--- src/tzip/tzip.c
+++ src/tzip/tzip.c
@@ -207,6 +207,11 @@ static int tzip_open(const char *path, struct fuse_file_info *fi)
 	handle->offset = 0;
 	handle->path = zippath;
 	handle->file = file;
+	handle->pbuf = NULL;
+	handle->from = 0;
+	handle->to = 0;
+	if (sem_init(&handle->lock, 0, 1) == -1)
+		_E("sem_init failed");
 
 #ifdef ARCH_32BIT
 	int hd = (int)handle;
@@ -250,8 +255,10 @@ static int tzip_read(const char *path, char *buf, size_t size, off_t offset,
 #endif
 
 	_D("Read - Path : %s  size : %zd offset : %jd ", path, size, offset);
+	sem_wait(&handle->lock);
 	ret = read_zipfile(handle, buf, size, offset);
-
+	sem_post(&handle->lock);
+	_I("Read ret = %d", ret);
 	return ret;
 }
 
@@ -274,6 +281,9 @@ static int tzip_release(const char *path, struct fuse_file_info *fi)
 #else
 	handle = (struct tzip_handle *)(fi->fh);
 #endif
+
+	if (sem_destroy(&handle->lock) == -1)
+		_E("sem_destroy failed");
 
 	unzCloseCurrentFile(handle->zipfile);
 	unzClose(handle->zipfile);
